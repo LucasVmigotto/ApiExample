898935340bcb5569b4f4447848b3ec231accc40f
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-10 14:29:56 -0300

    Pequenas correcoes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,10 +4,10 @@ import * as controller from '../controlles/usercontroller'
 export default (app: Express) => {
   app.route('/user')
     .get(controller.list)
-    .post(controller.create);
-  app.route('/user:id')
+    .post(controller.create)
+    .put(controller.update);
+  app.route('/user/:id')
     .get(controller.read)
-    .put(controller.update)
     .delete(controller.remove);
   app.use((req: Request, res: Response) => {
     res.status(404).send({ data: `${req.originalUrl} not found.` })
