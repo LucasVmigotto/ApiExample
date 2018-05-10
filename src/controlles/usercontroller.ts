a6ed3ecbef375ad751488db6506c550c9aaded0d
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-10 13:40:14 -0300

    Controller e Rotas adicionados

---
+++ src/controlles/usercontroller.ts
@@ -0,0 +1,12 @@
+import { default as User, UserModel } from '../models/user';
+import { Request, Response } from 'express';
+
+exports.list = (req: Request, res: Response) => {}
+
+exports.create = (req: Request, res: Response) => {}
+
+exports.read = (req: Request, res: Response) => {}
+
+exports.update = (req: Request, res: Response) => {}
+
+exports.delete = (req: Request, res: Response) => {}
