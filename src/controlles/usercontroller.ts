627878ef43d58e682d8a49459363ef754a6e64b0
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-11 10:58:17 -0300

    Pequenas correcoes

--- src/controlles/usercontroller.ts
+++ src/controlles/usercontroller.ts
@@ -9,7 +9,7 @@ const errorHandler = (err: Error, res: Response, message: String) => {
 
 export const list = (req: Request, res: Response) => {
   User.find((err: Error, users) => {
-    if (err) { errorHandler(err, res, 'Error listing the users.'); }
+    if (err) { return errorHandler(err, res, 'Error listing the users.'); }
     res.json({ data: users, err: false });
   });
 }
@@ -17,14 +17,14 @@ export const list = (req: Request, res: Response) => {
 export const create = (req: Request, res: Response) => {
   const newUser = new User(req.body);
   newUser.save((err: Error, user) => {
-    if (err) { errorHandler(err, res, 'Error saving the user.'); }
+    if (err) { return errorHandler(err, res, 'Error saving the user.'); }
     res.json({ data: user, err: false });
   });
 }
 
 export const read = (req: Request, res: Response) => {
   User.findById({ _id: req.params.id }, (err: Error, user) => {
-    if (err) { errorHandler(err, res, 'Error reading the user.'); }
+    if (err) { return errorHandler(err, res, 'Error reading the user.'); }
     res.json({ data: user, err: false });
   });
 }
@@ -37,14 +37,14 @@ export const update = (req: Request, res: Response) => {
     birthday: req.body.birthday,
     update: Date.now()
   }, { new: true }, (err: Error, user) => {
-    if (err) { errorHandler(err, res, 'Error updating the user.'); }
+    if (err) { return errorHandler(err, res, 'Error updating the user.'); }
     res.json({ data: user, err: false });
   });
 }
 
 export const remove = (req: Request, res: Response) => {
   User.remove({ _id: req.params.id }, (err: Error) => {
-    if (err) { errorHandler(err, res, 'Error deleting the user'); }
+    if (err) { return errorHandler(err, res, 'Error deleting the user'); }
     res.json({ err: false });
   });
 }
