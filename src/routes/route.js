4098224a527d01e95479ad75bfb0334aa2ab9eae
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-18 11:56:51 -0300

    Rotas definidas

---
+++ src/routes/route.js
@@ -0,0 +1,12 @@
+const graphqlHttl = require('express-graphql')
+const schema = require('../graphql/schema')
+module.exports = (app) => {
+  app.use('/user', graphqlHttl({
+    schema: schema.user,
+    graphiql: process.env.NODE_ENV === 'development'
+  }))
+
+  app.use((req, res) => {
+    res.status(404).send({ data: `${req.originalUrl} not found`})
+  })
+}
