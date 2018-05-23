fd1e85d6cdc9a2d0522556bd417a862329648327
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-23 11:18:00 -0300

    Mensagem de conexao com o banco adicionada

--- src/app.js
+++ src/app.js
@@ -2,26 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyparser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./configs/db.json')
 const routes = require('./routes/routes')
 const port = process.env.PORT || 8000
 const app = express()
-
-mongoose.connect(config.url).then(
-  () => {
-    console.log('\n****\tConnecting with the database...\n')
-  }
-).catch(
-  (err) => {
-    if (err) {
-      console.log(`\n****\tError while connecting to the database.\n` +
-      '****\t\tMake sure the mongodb service is started.\n' +
-      '****\t\tTo fix this, try run in the terminal \'sudo service mongo start\'' +
-      `****\tError description: ${err.message}\n`)
-    }
-  }
-)
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
+const mongoose = require('mongoose')
+const config = require('./configs/db.json')
+const log = require('./util/logLabel')
 const app = require('./app')
 
-const server = app.listen(app.get('port'), (err) => {
-  if (err) {
-    console.log(`\n****\tA errot occrred while running the application.\n` +
-      `****\tError description: ${err.message}`)
+let server = null
+
+mongoose.connect(config.url).then(
+  () => {
+    console.log(`${log()}Connected with the database...`)
+    server = app.listen(app.get('port'), (err) => {
+      if (err) {
+        console.log(`${log()}A error occrred while running the application.` +
+          `${log()}Error description: ${err.message}`)
+      }
+      console.log(`${log()}Application running` +
+        `${log()}URL: https://localhost:${app.get('port')}` +
+        `${log()}Environment: ${app.get('env')}`)
+    })
   }
-  console.log(`\n****\tApplication running\n` +
-    `\tURL: https://localhost:${app.get('port')}\n` +
-    `\tEnvironment: ${app.get('env')}`)
-})
+).catch(
+  (err) => {
+    if (err) {
+      console.log(`${log()}Error while connecting to the database.` +
+      `${log()}Make sure the mongodb service is started.` +
+      `${log()}To fix this, try run in the terminal 'sudo service mongo start'` +
+      `${log()}Error description: ${err.message}`)
+    }
+  }
+)
 
 module.exports = server
