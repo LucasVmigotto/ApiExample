0de462123062e5eb640ea9aff09d90816a0fab87
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-23 10:43:03 -0300

    Correcao de tipos e nomes

--- src/controlles/usercontroller.ts
+++ src/controlles/usercontroller.ts
@@ -8,7 +8,7 @@ const errorHandler = (err: Error, res: Response, message: String) => {
 }
 
 export const list = (req: Request, res: Response) => {
-  User.find((err: Error, users) => {
+  User.find((err: Error, users: UserModel) => {
     if (err) { return errorHandler(err, res, 'Error listing the users.'); }
     res.json({ data: users, err: false });
   });
@@ -16,26 +16,28 @@ export const list = (req: Request, res: Response) => {
 
 export const create = (req: Request, res: Response) => {
   const newUser = new User(req.body);
-  newUser.save((err: Error, user) => {
+  newUser.save((err: Error, user: UserModel) => {
     if (err) { return errorHandler(err, res, 'Error saving the user.'); }
     res.json({ data: user, err: false });
   });
 }
 
 export const read = (req: Request, res: Response) => {
-  User.findById({ _id: req.params.id }, (err: Error, user) => {
+  User.findById({ _id: req.params.id }, (err: Error, user: UserModel) => {
     if (err) { return errorHandler(err, res, 'Error reading the user.'); }
     res.json({ data: user, err: false });
   });
 }
 
 export const update = (req: Request, res: Response) => {
-  let user = req.body;
-  user.update = Date.now();
-  User.findByIdAndUpdate({ _id: req.body.id }, user, { new: true }, (err: Error, user) => {
-    if (err) { return errorHandler(err, res, 'Error updating the user.'); }
-    res.json({ data: user, err: false });
-  });
+  let newUser = req.body;
+  newUser.update = Date.now();
+  User.findByIdAndUpdate({ _id: req.body.id },
+    newUser, { new: true }, (err: Error, user: UserModel) => {
+      if (err) { return errorHandler(err, res, 'Error updating the user.'); }
+      res.json({ data: user, err: false });
+    }
+  );
 }
 
 export const remove = (req: Request, res: Response) => {
