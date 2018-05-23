70b964f754f5cfb0330a8484051d193e625101d0
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-05-23 12:12:08 -0300

    Mensagem de conexao com o banco adicionada

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
+const mongoose = require('mongoose')
+const { db } = require('./configs/config.json')
+const log = require('./util/logLabel')
 const app = require('./app')
-const label = '\n****\t'
 
-const server = app.listen(app.get('port'), (err) => {
+let server = null
+
+mongoose.connect(db.url).then(() => {
+  console.log(`${log()}Connected with the database.`)
+  server = app.listen(app.get('port'), (err) => {
+    if (err) {
+      console.log(`${log()} Error occurred while running the application.` +
+        `${log()}Description: ${err.message}`)
+    }
+    console.log(`${log()}Application running...` +
+      `${log()}URL: http://localhost:${app.get('port')}` +
+      `${log()}Environment: ${app.get('env')}`)
+  })
+}).catch((err) => {
   if (err) {
-    console.log(`${label} Error occurred while running the application.` +
-      `${label}Description: ${err.message}`)
+    console.log(`${log()}Error connecting to the database.` +
+      `${log()}Make sure the mongo service is started.` +
+      `${log()}To fix this, try run 'sudo service mongod start' on terminal` +
+      `${log()}Error description: ${err.message}`)
   }
-  console.log(`${label}Application running...` +
-    `${label}URL: http://localhost:${app.get('port')}` +
-    `${label}Environment: ${app.get('env')}`)
 })
 
 module.exports = server
